180c85ca529c3b6b8bd30741cbcac9473941590a
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2024-12-22 19:51:14 +0300

    minor settings for collection User

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -952,7 +952,6 @@ export interface PluginUsersPermissionsUser
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -966,6 +965,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    fullName: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
